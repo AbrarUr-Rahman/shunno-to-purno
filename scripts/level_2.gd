dfbc25cbee48361b414e91269d0e592db3e1a91e
Nasir Uddin <nasiruddin8476@gmail.com>
2024-11-19 18:36:02 +0600

    Added new scans level for the game

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -3,12 +3,5 @@ extends Control
 
 
 
-
-	
-
-
-
-
-
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/female_level_5.tscn")
